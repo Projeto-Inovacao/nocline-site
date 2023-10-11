504708690418c0c8733984c9c4a54a16aa5ee9a3
jonatao37 <jonathan.almeida@sptech.school>
2023-10-11 13:35:40 -0300

    botao finalizado

--- public/js/script_cadastro.js
+++ public/js/script_cadastro.js
@@ -5,10 +5,11 @@ const nextBtnSec = document.querySelector(".next-1");
 const prevBtnThird = document.querySelector(".prev-2");
 const nextBtnThird = document.querySelector(".next-2");
 const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
 const progressText = [...document.querySelectorAll(".step p")];
 const progressCheck = [...document.querySelectorAll(".step .check")];
 const bullet = [...document.querySelectorAll(".step .bullet")];
-let max = 4;
+let max = 3;
 let current = 1;
 
 nextBtnFirst.addEventListener("click", function(){
@@ -19,7 +20,7 @@ nextBtnFirst.addEventListener("click", function(){
   current += 1;
 });
 nextBtnSec.addEventListener("click", function(){
-  slidePage.style.marginLeft = "-30%";
+  slidePage.style.marginLeft = "-50%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -34,7 +35,7 @@ prevBtnSec.addEventListener("click", function(){
   current -= 1;
 });
 prevBtnThird.addEventListener("click", function(){
-  slidePage.style.marginLeft = "-30%";
+  slidePage.style.marginLeft = "-25%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
