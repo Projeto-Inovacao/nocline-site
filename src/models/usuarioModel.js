6efa17ecd2593c5ab5b7255a74bafd51f56008b0
GyUliAmArTinsPiquEira <gyulia.piqueira@sptech.school>
2023-10-25 09:08:46 -0300

    a

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -6,16 +6,11 @@ var database = require("../database/config")
     //WHERE (email = '${email}' AND senha = sha('${senha}',256)) ;/
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-<<<<<<< HEAD
     var instrucao = `
     select 
     * from colaborador 
      join empresa on fk_empresa = id_empresa
     WHERE (email = '${email}' AND senha = '${senha}') ;
-=======
-    var instrucao = `select * from colaborador join empresa on fk_empresa = id_empresa WHERE email = 'no@email.com' AND senha = 'senha321' ;
-   
->>>>>>> 7a813cb9a7240a46333afb1e34be722cb771faab
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     
