b274fbc288ac71851775a1d0ff6ee2457ca86ab6
Rita-de-CassiaB <Rita.Barbosa@sptech.school>
2023-11-18 16:12:48 -0300

    projeto rita

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var processosRouter = require("./src/routes/processos");
+var redeRouter = require("./src/routes/rede");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +30,7 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/processos", processosRouter);
+app.use("/rede", redeRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
