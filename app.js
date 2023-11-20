c4637b4c4df4de0bf284599740e1308826b1d201
Marcos-Floriano <marcos.feu@sptech.school>
2023-11-20 13:45:01 -0300

    Rendimento do processador

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var processosRouter = require("./src/routes/processos");
+var processadorRouter = require("./src/routes/processador");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +30,7 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/processos", processosRouter);
+app.use("/processador", processadorRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
--- src/database/config.js
+++ src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "nocline",
-    user: "noc_line",
-    password: "noc_line134#",
+    user: "root",
+    password: "Marnn111",
 };
 
 function executar(instrucao) {
