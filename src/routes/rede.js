bb0ce42877c914b2b60b031fa0cbeb3b9cbc8be2
Rita-de-CassiaB <Rita.Barbosa@sptech.school>
2023-11-22 18:15:09 -0300

    Completando

--- src/routes/rede.js
+++ src/routes/rede.js
@@ -12,5 +12,12 @@ router.get("/tempo-realRede/:idMaquina", function (req, res) {
     redeController.buscarMedidasEmTempoRealRede(req, res);
 })
 
+router.get("/ultimasDesempenho/:idMaquina", function (req, res) {
+    redeController.buscarUltimasMedidasDesempenhoR(req, res);
+});
+
+router.get("/tempo-realDesempenho/:idMaquina", function (req, res) {
+    redeController.buscarMedidasEmTempoRealDesempenhoR(req, res);
+})
 
 module.exports = router;
