309a4b5f95db5d4ee80ab8546865aecc71543260
Marcos-Floriano <marcos.feu@sptech.school>
2023-11-30 13:29:06 -0300

    Finalixando Dash Rendimento do processador

--- src/controllers/processadorController.js
+++ src/controllers/processadorController.js
@@ -42,6 +42,70 @@ function buscarUltimasMedidasFrequencia(req, res) {
     });
 }
 
+
+function buscarUltimasCpuporProcessos(req, res) {
+
+    const limite_linhas = 7;
+
+    var idMaquina = req.params.idMaquina;
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    medidaModel.buscarUltimasCpuporProcessos(idMaquina, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarProcessador(req, res) {
+
+    const limite_linhas = 7;
+
+    var idMaquina = req.params.idMaquina;
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    medidaModel.listarProcessador(idMaquina, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarUltimasDiscoporProcessos(req, res) {
+
+    const limite_linhas = 7;
+
+    var idMaquina = req.params.idMaquina;
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    medidaModel.buscarUltimasDiscoporProcessos(idMaquina, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 // KPI USO PROCESSADOR MARCOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSZ
 function buscarMaiorUso(req, res) {
 
@@ -66,6 +130,9 @@ function buscarMaiorUso(req, res) {
 
 module.exports = {
     buscarUltimasMedidasProcessadorTemp,
-    buscarUltimasMedidasFrequencia, 
-    buscarMaiorUso
+    buscarUltimasMedidasFrequencia,
+    buscarUltimasCpuporProcessos,
+    buscarUltimasDiscoporProcessos, 
+    buscarMaiorUso,
+    listarProcessador
 }
--- src/models/processadorModel.js
+++ src/models/processadorModel.js
@@ -17,6 +17,29 @@ function buscarUltimasMedidasProcessadorTemp(idMaquina, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
+function buscarUltimasCpuporProcessos(idMaquina, limite_linhas) {
+    instrucaoSql = `SELECT nome_processo, AVG(uso_cpu) as media_uso_cpu FROM processos WHERE fk_maquinaP = ${idMaquina} GROUP BY nome_processo ORDER BY media_uso_cpu DESC limit 7;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function buscarUltimasDiscoporProcessos(idMaquina, limite_linhas) {
+    instrucaoSql = `SELECT nome_processo, SUM(gravacao_disco) as total_uso_disco FROM processos WHERE fk_maquinaP = ${idMaquina} GROUP BY nome_processo ORDER BY total_uso_disco DESC limit 7;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function listarProcessador(idMaquina, limite_linhas) {
+    instrucaoSql = `select identificador,fabricante,microarquitetura,fk_maquina_especificacao,fk_empresa_especificacao from especificacao WHERE fk_maquina_especificacao = ${idMaquina} limit 1;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 function buscarMaiorUso(idMaquina) {
     instrucaoSql = `
     SELECT CONCAT(ROUND((MAX(dado_coletado) / 100) * 100), '%') AS porcentagem FROM monitoramento WHERE descricao = 'cpu individual marcos'  AND fk_maquina_monitoramento = ${idMaquina};`;
@@ -29,5 +52,8 @@ function buscarMaiorUso(idMaquina) {
 module.exports = {
     buscarUltimasMedidasProcessadorTemp,
     buscarUltimasMedidasFrequencia, 
-    buscarMaiorUso
+    buscarMaiorUso,
+    buscarUltimasCpuporProcessos,
+    buscarUltimasDiscoporProcessos,
+    listarProcessador
 }
