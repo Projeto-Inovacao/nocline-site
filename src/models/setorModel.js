b0ea5436d31fd1ac7ec908e82247409b5f5621bb
Luize <125298386+LuizeCarilloF@users.noreply.github.com>
2023-11-30 11:07:42 -0300

    graficos pegando e tempo legal

--- src/models/setorModel.js
+++ src/models/setorModel.js
@@ -40,11 +40,11 @@ function buscarMedidasEmTempoRealCPU() {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select * from VW_MEDIA_CPU_POR_SETOR_E_LINHA
+        instrucaoSql = `select * from VW_MEDIA_CPU_POR_LINHA_CCO
         ORDER BY  setor DESC limit 5;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select * from VW_MEDIA_CPU_POR_SETOR_E_LINHA
+        instrucaoSql = `select * from VW_MEDIA_CPU_POR_LINHA_CCO
         ORDER BY  setor DESC limit 5;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
